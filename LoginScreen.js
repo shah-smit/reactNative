68ec80305db5410d78e94b484189348e46a23937
raja95shah <Smit@Smits-MacBook-Pro.local>
2016-01-19 23:04:01 +0800

    adding new file for stylesheet

--- LoginScreen.js
+++ LoginScreen.js
@@ -6,16 +6,13 @@ var {Text,View,TextInput,TouchableWithoutFeedback,Image,ToastAndroid,Platform,Na
 
 
 var MainActivity = require('./MainActivity');
+var styles = require('./TrabbleStyleSheet');
 
 class LoginScreen extends React.Component {
 
     Login(){
 
-      return  <Navigator
-            initialRoute={{name: 'MainActivity', component: MainActivity, index: 0}}
-            renderScene={(route, navigator) =>    {
-    return React.createElement(<MainActivity />);
-  }} />
+
         }
 
 
@@ -36,64 +33,6 @@ class LoginScreen extends React.Component {
 
 }
 
-var styles = React.StyleSheet.create({
-    image:{
-        height: 150,
-        alignSelf: "center",
-        marginTop: 50,
-        opacity: 1
-    },
-    loginView:{
-      backgroundColor: "#FA8A3A",
-        flex: 1
-    },
-    loginText: {
-        fontSize: 50,
-        color: "white",
-        marginTop: 10,
-        alignSelf: "center"
-    },
-    usernameText: {
-        height: 40,
-        color: 'black',
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginTop: 10
-    },
-    passwordText: {
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginTop: 10
-    },
-    loginButtonText:{
-        alignSelf: 'center',
-        fontSize: 20,
-        color: 'white'
-
-    },
-    loginButton:{
-        marginTop: 10,
-        height: 30,
-        backgroundColor: 'blue'
-    },
-    signUpButtonText:{
-        alignSelf: 'center',
-        fontSize: 20,
-        color: 'white'
-
-    },
-    signUpButton:{
-        marginTop: 10,
-        height: 30,
-        backgroundColor: 'grey'
-    },
-    forgetPasswordText:{
-        fontSize: 10,
-        alignSelf: 'center',
-        marginTop: 10
-    }
-});
 
 //React.AppRegistry.registerComponent('loginScreen', function() { return loginScreen });
 module.exports = LoginScreen;
---
+++ TrabbleStyleSheet.js
@@ -0,0 +1,70 @@
+/**
+ * Created by Smit on 19/1/16.
+ */
+
+'use strict';
+
+
+var React = require('react-native');
+var {Text,View,TextInput} = React;
+
+var styles = React.StyleSheet.create({
+    image:{
+        height: 150,
+        alignSelf: "center",
+        marginTop: 50,
+        opacity: 1
+    },
+    loginView:{
+        backgroundColor: "#FA8A3A",
+        flex: 1
+    },
+    loginText: {
+        fontSize: 50,
+        color: "white",
+        marginTop: 10,
+        alignSelf: "center"
+    },
+    usernameText: {
+        height: 40,
+        color: 'black',
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 10
+    },
+    passwordText: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 10
+    },
+    loginButtonText:{
+        alignSelf: 'center',
+        fontSize: 20,
+        color: 'white'
+
+    },
+    loginButton:{
+        marginTop: 10,
+        height: 30,
+        backgroundColor: 'blue'
+    },
+    signUpButtonText:{
+        alignSelf: 'center',
+        fontSize: 20,
+        color: 'white'
+
+    },
+    signUpButton:{
+        marginTop: 10,
+        height: 30,
+        backgroundColor: 'grey'
+    },
+    forgetPasswordText:{
+        fontSize: 10,
+        alignSelf: 'center',
+        marginTop: 10
+    }
+});
+
+module.exports = TrabbleStyleSheet;
