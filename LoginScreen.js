5408a64bd3dd3926e2381c5df455954ee17c8361
raja95shah <Smit@Smits-MacBook-Pro.local>
2016-01-20 00:19:38 +0800

    checking for the login details but not succesful

--- LoginScreen.js
+++ LoginScreen.js
@@ -10,22 +10,32 @@ var styles = require('./TrabbleStyleSheet');
 
 class LoginScreen extends React.Component {
 
-    Login(){
 
-        this.props.navigator.push({
-            component: MainActivity
-        })
+
+    Login(username,password){
+        if(username!=''||password!=''){
+            alert('Empty Input Found');
+        }
+        else{
+            this.props.navigator.push({
+                component: MainActivity
+            })
+        }
+
         }
 
 
 
     render() {
+
+        var username;
+        var password;
         return<View style={styles.loginView}>
             <Image style={styles.image} source={require('./Ionic.png')}/>
             <Text style={styles.loginText}>Chat System</Text>
-        <TextInput style={styles.usernameText} placeholder="username" placeholderTextColor="black" ></TextInput>
-            <TextInput style={styles.passwordText} placeholder="password" placeholderTextColor="black" secureTextEntry></TextInput>
-        <TouchableWithoutFeedback  onPress={ () => this.Login() }><View style={styles.loginButton}  ><Text style={styles.loginButtonText}>Log in</Text></View></TouchableWithoutFeedback>
+        <TextInput style={styles.usernameText} placeholder="username" placeholderTextColor="black" onChangeText={(username) => this.setState({username})} value={this.state.username}/>
+            <TextInput style={styles.passwordText} placeholder="password" placeholderTextColor="black" onChangeText={(password) => this.setState({password})} value={this.state.password} secureTextEntry/>
+        <TouchableWithoutFeedback  onPress={ () => this.Login(username,password) }><View style={styles.loginButton}  ><Text style={styles.loginButtonText}>Log in</Text></View></TouchableWithoutFeedback>
             <TouchableWithoutFeedback><View style={styles.signUpButton}><Text style={styles.signUpButtonText}>Sign Up</Text></View></TouchableWithoutFeedback>
             <TouchableWithoutFeedback><View><Text style={styles.forgetPasswordText}>Forgot password?</Text></View></TouchableWithoutFeedback>
         </View>
