7cc83f5b08d596120ef7172b0c4d3064b6b4d0f6
raja95shah <Smit@Smits-MacBook-Pro.local>
2016-01-19 18:29:45 +0800

    Add NavigatorIOS

--- LoginScreen.js
+++ LoginScreen.js
@@ -2,31 +2,34 @@
 
 
 var React = require('react-native');
-var {Text,View,TextInput,TouchableWithoutFeedback,Image,ToastAndroid,Platform,Navigator} = React;
+var {Text,View,TextInput,TouchableWithoutFeedback,Image,ToastAndroid,Platform,NavigatorIOS,Navigator} = React;
 
 
-
+var MainActivity = require('./MainActivity');
 
 class LoginScreen extends React.Component {
-    CustomSettingForPlatform(){
-        if(Platform.OS === 'ios'){
-           //return '{() => VibrationIOS.vibrate()}'
-            alert('Somethinf');
-            return
-        }
-        else{
-            return ToastAndroid.show('This is a toast with long duration', ToastAndroid.LONG)
 
+    Login(){
+        alert();
+       
+        return <NavigatorIOS
+            initialRoute={{
+        component: MainActivity,
+        title: 'MainActivity',
+        passProps: { myProp: 'MainActivity' },
+      }}
+        />
         }
 
-    }
+
+
     render() {
         return<View style={styles.loginView}>
             <Image style={styles.image} source={require('./Ionic.png')}/>
             <Text style={styles.loginText}>Chat System</Text>
         <TextInput style={styles.usernameText} placeholder="username" placeholderTextColor="black"></TextInput>
             <TextInput style={styles.passwordText} placeholder="password" placeholderTextColor="black" secureTextEntry></TextInput>
-        <TouchableWithoutFeedback  onPress={this.CustomSettingForPlatform.bind(this)}><View style={styles.loginButton}  ><Text style={styles.loginButtonText}>Smit is smart</Text></View></TouchableWithoutFeedback>
+        <TouchableWithoutFeedback  onPress={ () => this.Login() }><View style={styles.loginButton}  ><Text style={styles.loginButtonText}>Smit is smart</Text></View></TouchableWithoutFeedback>
             <TouchableWithoutFeedback><View style={styles.signUpButton}><Text style={styles.signUpButtonText}>Sign Up</Text></View></TouchableWithoutFeedback>
             <TouchableWithoutFeedback><View><Text style={styles.forgetPasswordText}>Forgot password?</Text></View></TouchableWithoutFeedback>
         </View>
@@ -95,5 +98,5 @@ var styles = React.StyleSheet.create({
     }
 });
 
-React.AppRegistry.registerComponent('loginScreen', function() { return loginScreen });
+//React.AppRegistry.registerComponent('loginScreen', function() { return loginScreen });
 module.exports = LoginScreen;
