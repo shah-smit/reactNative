d4c2626afc5d2cda3954321870618cbff976cf9e
raja95shah <Smit@Smits-MacBook-Pro.local>
2016-01-20 00:53:48 +0800

    checking for the login details but not succesful

--- LoginScreen.js
+++ LoginScreen.js
@@ -2,25 +2,23 @@
 
 
 var React = require('react-native');
-var {Text,View,TextInput,TouchableWithoutFeedback,Image,ToastAndroid,Platform,NavigatorIOS,Navigator} = React;
+var {Text,View,TextInput,TouchableWithoutFeedback,Image,TouchableOpacity,Platform,NavigatorIOS,Navigator} = React;
 
 
 var MainActivity = require('./MainActivity');
 var styles = require('./TrabbleStyleSheet');
 
 class LoginScreen extends React.Component {
+    var username,password;
+
+    LoginScreen(username,password){
+        username = username;
+        password = password;
+    }
 
 
-
-    Login(username,password){
-        if(username!=''||password!=''){
-            alert('Empty Input Found');
-        }
-        else{
-            this.props.navigator.push({
-                component: MainActivity
-            })
-        }
+    Login(){
+        alert(username);
 
         }
 
@@ -28,14 +26,12 @@ class LoginScreen extends React.Component {
 
     render() {
 
-        var username;
-        var password;
         return<View style={styles.loginView}>
             <Image style={styles.image} source={require('./Ionic.png')}/>
             <Text style={styles.loginText}>Chat System</Text>
-        <TextInput style={styles.usernameText} placeholder="username" placeholderTextColor="black" onChangeText={(username) => this.setState({username})} value={this.state.username}/>
-            <TextInput style={styles.passwordText} placeholder="password" placeholderTextColor="black" onChangeText={(password) => this.setState({password})} value={this.state.password} secureTextEntry/>
-        <TouchableWithoutFeedback  onPress={ () => this.Login(username,password) }><View style={styles.loginButton}  ><Text style={styles.loginButtonText}>Log in</Text></View></TouchableWithoutFeedback>
+            <TextInput style={styles.usernameText} placeholder="username" placeholderTextColor="black" onChangeText={(username) => this.setState({username})} value={this.state.username}/>
+            <TextInput style={styles.passwordText} placeholder="password" placeholderTextColor="black" onChangeText={(password) => this.setState({password})} value={this.state.password} onSubmitEditing={this.Login} secureTextEntry/>
+            <TouchableWithoutFeedback onPress={this.Login}><View style={styles.loginButton}> <Text style={styles.loginButtonText}>Sign In</Text></View></TouchableWithoutFeedback>
             <TouchableWithoutFeedback><View style={styles.signUpButton}><Text style={styles.signUpButtonText}>Sign Up</Text></View></TouchableWithoutFeedback>
             <TouchableWithoutFeedback><View><Text style={styles.forgetPasswordText}>Forgot password?</Text></View></TouchableWithoutFeedback>
         </View>
@@ -47,3 +43,8 @@ class LoginScreen extends React.Component {
 
 
 module.exports = LoginScreen;
+/*
+ this.props.navigator.push({
+ component: MainActivity
+ })
+ */
